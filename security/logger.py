025f67ca3606e969cf779d2abf754f822894d9ca
Niang931 <kytnlmw1226@gmail.com>
2026-07-07 18:53:59 +0800

    logging for debug purpose

--- security/logger.py
+++ security/logger.py
@@ -5,7 +5,7 @@ logger.setLevel(logging.INFO)
 
 file_handler = logging.FileHandler('security/app.log',
                                    mode='a', encoding='utf-8')
-file_handler.setLevel(logging.WARNING)
+file_handler.setLevel(logging.INFO)
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s %(message)s')
 file_handler.setFormatter(formatter)
